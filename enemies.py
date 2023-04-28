b58741f4cd7b2ddf96b3c69bf986ee4086069106
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2023-04-28 14:18:07 +0300

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -2,35 +2,9 @@ import pygame
 pygame.init()
 w, h = 1000, 980
 
-class Enemies:
+def calizia(x, y, hw):
+    return pygame.Rect(x, y, hw, hw)
 
-    def __init__(self, window, x, y, hw, speed, live, D_list):
-        self.window = window
-        self.x = x
-        self.y = y
-        self.hw = hw
-        self.speed = speed
-        self.live = live
-        self.D_list = D_list
-
-        self.rect = pygame.Rect(self.x, self.y, self.hw, self.hw)
-
-    def dead(self):
-        if self.live <= 0:
-            self.D_list.remove(self)
-            
-    #move to player||Px - player X, Py - player y
-    def move(Px, Py):
-        if self.x > Px:
-            self.x -= self.speed
-        elif self.x < Px:
-            self.x += self.speed
-
-        if self.y > Px:
-            self.y -= self.speed
-        elif self.y < Px:
-            self.y += self.speed
-
-
-    def draw(self):
-        pygame.draw.rect(self.window, (130, 0, 0), self.rect)
+def move(rec, px, py, speed):
+    rec.x += speed
+    return pygame.Rect(rec.x, rec.y, rec.h, rec.w)
